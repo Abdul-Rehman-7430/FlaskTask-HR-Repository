4250e1d8e392843884e4ac255a730baea5a8d772
basit umair <basitumair@gmail.com>
2022-05-26 20:32:53 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, redirect, url_for, request, render_template,make_response, send_file,jsonify
+import nltk
+import spacy
+
+app = Flask(__name__)
+@app.route('/index')
+def index():
+    return ("--Hello word--")
+
+
+
+if __name__ == '__main__':
+    app.run()
